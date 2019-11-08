4f9434c269c055cccce4db0f575ac2f097b80ec1
Marco Aguilar <marcoaguilar@marcos-mbp.home>
2019-11-07 17:34:13 -0800

    data.txt file was edited all that is needed are the pixel coordinates for each stadium on the map. Baseball_League_Stadium.txt was removed since data.txt is the improved version of it. r2.cpp is the code for requirement 2, it is still not finalized and also contains code for reading from files to update our Container class full of Stadium objects. Stadium.h has been modified and stadium2.h has been added for staium.h It will be renamed to stadium.cpp later.

--- stadium.h
+++ stadium.h
@@ -1,82 +1,69 @@
 #ifndef STADIUM_H
 #define STADIUM_H
-#include<string>
-#include<stack>
-#include<vector>
+
+#include <string>
+#include <stack>
+#include <iostream>
+
 using namespace std;
-class stadium{
+
+class Stadium{
 private:
+    char stadium_type;
     string stadium_name;
-    string team_teame;
+    string team_name;
     string street_address;
     string city_state_zip;
     string box_office;
     string date_opened;
     int seating_capacity;
 public:
-    stadium(){
-        stadium_name="N/A";
-        team_teame="N/A";
-        street_address="N/A";
-        city_state_zip="N/A";
-        box_office="N/A";
-        date_opened="N/A";
-        seating_capacity=0;
-    }
-    void set_stadium_name(string a){
-        stadium_name=a;
-    }
+    //DEFAULT CTOR
+    Stadium()
+    //FULL CTOR
+    Stadium(char type,
+            string stadiumName,
+            string teamName,
+            string streetAddress,
+            string cityStateZip,
+            string boxOffice,
+            string dateOpened,
+            int seatingCapacity);
+    //COPY CTOR
+    Stadium(const Stadium& source);
 
-    void set_team_name(string a){
-        team_teame=a;
-    }
+    void set_stadium_name(string a){stadium_name=a;}
 
-    void set_street_address(string a){
-        street_address=a;
-    }
+    void set_team_name(string a){team_name=a;}
 
-    void set_city_state_zip(string a){
-        city_state_zip=a;
-    }
+    void set_street_address(string a){street_address=a;}
 
-    void set_box_office(string a){
-        box_office=a;
-    }
+    void set_city_state_zip(string a){city_state_zip=a;}
 
-    void set_date_opened(string a){
-        date_opened=a;
-    }
+    void set_box_office(string a){box_office=a;}
 
-    void set_seating_capacity(int a){
-        seating_capacity=a;
-    }
+    void set_date_opened(string a){date_opened=a;}
 
-    string get_stadium_name(){
-        return stadium_name;
-    }
+    void set_seating_capacity(int a){seating_capacity=a;}
 
-    string get_team_name(){
-        return team_teame;
-    }
+    string get_stadium_name(){return stadium_name;}
 
-    string get_street_address(){
-        return street_address;
-    }
+    string get_team_name(){return team_teame;}
 
-    string get_city_state_zip(){
-        return city_state_zip;
-    }
+    string get_street_address(){return street_address;}
 
-    string get_box_office(){
-        return box_office;
-    }
+    string get_city_state_zip(){return city_state_zip;}
 
-    string get_date_opened(){
-        return date_opened;
-    }
+    string get_box_office(){return box_office;}
 
-    int get_seating_capacity(){
-        return seating_capacity;
-    }
+    string get_date_opened(){return date_opened;}
+
+    int get_seating_capacity(){return seating_capacity;}
+
+    //COUT OPERATOR
+    ostream& friend operator<<(ostream out, const Stadium& stadium);
+
+    //ASSIGNMENT OPERATOR
+    operator=();
 };
 #endif // STADIUM_H
